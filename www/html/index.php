fffb9140ca593dc9c7bbc6530865441e7c810695
zhuzhenyu <zhuzhenyu307@gmail.com>
2017-11-24 00:10:55 +0800

    first commit

---
+++ www/html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Env Run In Docker</title>
+</head>
+<body>
+    I's Ok
+    <br>
+    <?= $_SERVER['REMOTE_ADDR'];?>
+</body>
+</html>
